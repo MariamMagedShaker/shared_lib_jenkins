4275c8c405bc16036ec6ca3c4fbc9601e5769bea
Mariam Maged <maryam.maged76@gmail.com>
2025-05-27 01:55:17 +0300

    python shared

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,4 +1,9 @@
 
-def call(String NAME = "ITI NOW"){
-    sh "echo \"Hello ${NAME}\" "
+def call(name = "ITI NOW") {
+    echo ">>> DEBUG: Received name = ${name}"
+    echo "sayHello: Hello ${name}"
 }
+
+// def call( NAME = "ITI NOW"){
+//     sh "echo \"Hello ${NAME}\" "
+// }
